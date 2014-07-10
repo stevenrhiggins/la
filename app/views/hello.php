df7ee05dfa7fc7ee0414604bfcc261a5923bace9
Steven Higgins <shiggins@gmail.com>
2014-07-10 09:37:19 -0400

    initial commit

---
+++ app/views/hello.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>2014 Leadership Academy - Puerto Rico, October 23-25, 2014</title>
+	<style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+		@import url(//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			text-align:center;
+			color: #999;
+		}
+
+		.panel-top{
+			background: #4d6c9f;
+			height: 140px;
+			color: #fffefe;
+		}
+		.text-left{
+			text-align: left;
+			margin-top: 50px;
+		}
+		.text-right{
+			text-align: right;
+			margin-top: 50px;
+			font-size: 32px;
+		}
+		.panel-one{
+			background-image: url(images/Carson-Emmett1280x853.jpg);
+			background-repeat: no-repeat;
+			height: 853px;
+		}
+	</style>
+
+	<link rel="stylesheet" type="text/css" href="javascript/jquery.vegas.min.css">
+
+</head>
+<body>
+	<div class="container-fluid panel-top">
+		<div class="col-md-6 text-left">Presented by</div>
+		<div class="col-md-6 text-right">
+			<ul class="nav nav-pills">
+			  <li class="active"><a href="#">Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
+			  <li><a href="#">Speakers&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
+			  <li><a href="#">Schedule&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
+			  <li><a href="#">Location</a></li>
+			</ul>			
+		</div>
+	</div>
+	<div class="container-fluid panel-one">
+	</div>
+</body>
+</html>
+
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>	
+<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>	
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script>	
+<script type="text/javascript" src="javascript/jquery.vegas.min.js"></script>	
