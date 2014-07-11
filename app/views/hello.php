300adc2f3bf0e6bf1b10fae3d889775f4d3ccb97
Steven Higgins <shiggins@gmail.com>
2014-07-11 09:58:14 -0400

    update 7-11

--- app/views/hello.php
+++ app/views/hello.php
@@ -3,15 +3,16 @@
 <head>
 	<meta charset="UTF-8">
 	<title>2014 Leadership Academy - Puerto Rico, October 23-25, 2014</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">	
 	<style>
-		@import url(//fonts.googleapis.com/css?family=Lato:700);
-		@import url(//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css);
+		@import url(//fonts.googleapis.com/css?family=Lato:300,400,700);
 
 		body {
 			margin:0;
 			font-family:'Lato', sans-serif;
 			text-align:center;
-			color: #FFF;
+			color: #FFF !important;
+			font-weight: 400;
 		}
 
 		.panel-top{
@@ -32,7 +33,14 @@
 			height: 920px;
 		}
 		.content-one{
-			margin-top: 430px;
+			margin-top: 410px;	
+			margin-left: 75px;
+		}
+		.content-one-text{
+			font-size: 22.5px;
+			width: 765px;
+			margin-left: 20px;
+			padding: none;
 		}
 		.panel-two{
 			height: 910px;
@@ -40,9 +48,12 @@
 		.panel-three{
 			color: #000;
 		}
+		.schedule-text{
+			font-size: 24px;
+			font-weight: 300;
+		}
 	</style>
 
-	<link rel="stylesheet" type="text/css" href="javascript/jquery.vegas.min.css">
 
 </head>
 <body>
@@ -53,14 +64,18 @@
 			  <li class="active"><a href="#">Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
 			  <li><a href="#">Speakers&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
 			  <li><a href="#">Schedule&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
-			  <li><a href="#">Location</a></li>
+			  <li><a href="#">Location&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|</a></li>
+			  <li><a href="#">Register</a></li>
 			</ul>			
 		</div>
 	</div>
 
 	<div class="container-fluid panel-one">
-		<div class="col-md-6 col-md-offset-5 content-one">
-			<h2>Welcome to Peurto Rico</h2>
+		<div class="col-md-9 content-one text-right">
+			<h1>Welcome to Puerto Rico</h1>
+			<div class="row">
+				<div class="col-md-5 col-md-8-offset content-one-text pull-right">In its eighth year, the 2014 Leadership Academy is an opportunity for you to enrich your career and enhance your leadership skills. We'll inspire you to reach new heights with AFP and within the profession, while engaging with other AFP members. Leadership Academy is a members-only event and a benefit for leaders within the AFP community.<br>We look forward to seeing you in Puerto Rico in October!</div>
+			</div>
 		</div>
 	</div>
 
@@ -70,17 +85,18 @@
 	</div>	
 
 	<div class="container-fluid panel-three">
-		<div class="col-md-3">
-			<h3>Thursday, October 30, 2014</h3>
+		<div class="col-md-3 text-left">
+			<h2>Thursday, October 30, 2014</h2>
+			<div class="schedule-text">7:30 - 5:00 PM<br>Registration</div>
 		</div>
-		<div class="col-md-3">
-			<h3>Friday, October 30, 2014</h3>			
+		<div class="col-md-3 text-left">
+			<h2>Friday, October 30, 2014</h2>			
 		</div>
-		<div class="col-md-3">
-			<h3>Friday, October 30, 2014</h3>			
+		<div class="col-md-3 text-left">
+			<h2>Friday, October 30, 2014</h2>			
 		</div>
-		<div class="col-md-3">
-			<h3>Saturday, October 30, 2014</h3>			
+		<div class="col-md-3 text-left">
+			<h2>Saturday, October 30, 2014</h2>			
 		</div>
 	</div>
 
